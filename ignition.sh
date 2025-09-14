2c88882a53c81229b1c8b0f78af4f68c4242524a
Claudio Walser <info@gitcd.io>
2025-09-14 21:46:22 +0200

    Update ignition.sh

--- ignition.sh
+++ ignition.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "$WEBDAV_URL $WEBDAV_USER $WEBDAV_PASSWORD" >> /etc/davfs2/secrets
-mount -t davfs $WEBDAV_URL /Recordings/  -o nolocks
+mount -t davfs $WEBDAV_URL /Recordings/
 
 python /app/record-ffmpeg.py
